680e538a536d0479e4e3d5b1cd24541826262d52
Manuel Jung <manueljung@Manuels-MBP.fritz.box>
2018-11-30 17:22:42 +0100

    initial commit

---
+++ src/middleware.js
@@ -0,0 +1,88 @@
+// @flow 
+import type {AddRule, Store, Action} from './types'
+
+let listBefore = {}
+let listInstead = {}
+let listAfter = {}
+let buffer = new Map()
+let backlog = new Map()
+
+export const INSERT_BEFORE = 'INSERT_BEFORE'
+export const INSERT_INSTEAD = 'INSERT_INSTEAD'
+export const INSERT_AFTER = 'INSERT_AFTER'
+
+export default function middleware(store:Store<any>){
+  return (action:Action) => (next:Function) => {
+    let instead = false
+
+    listInstead[action.type] && listInstead[action.type].forEach((_,rule) => {
+      if(rule.condition(action)) {
+        instead = true
+        rule.consequence(store, action)
+      }
+    })
+
+    !instead && listBefore[action.type] && listBefore[action.type].forEach((_,rule) => {
+      if(rule.condition(action)) rule.consequence(store, action)
+    })
+
+    const result = instead ? null : next(action)
+
+    !instead && listAfter[action.type] && listAfter[action.type].forEach((_,rule) => {
+      if(rule.condition(action)) rule.consequence(store, action)
+    })
+
+    return result
+  }
+}
+
+export const addRule:AddRule<any> = (rule, options={}) => {
+  const add = list => {
+    const targets = Array.isArray(rule.target) ? rule.target : [rule.target]
+    // create missing keys
+    targets.forEach(target => {if(!list[target]) list[target] = new Map()})
+    // push rule
+    targets.forEach(target => {list[target].set(rule.id, rule)})
+  }
+
+  if(!options.addWhen)
+  switch(rule.position){
+    case INSERT_BEFORE: {add(listBefore); break}
+    case INSERT_INSTEAD: {add(listInstead); break}
+    case INSERT_AFTER: {add(listAfter); break}
+    default: break;
+  }
+
+  if(!options.addWhen){
+    buffer.set(rule.id, rule)
+  }
+
+  return rule
+}
+// export const removeRule = program => {
+//   switch(program.position){
+//     case INSERT_BEFORE: return listBefore = listBefore.filter(o => o !== program)
+//     case INSERT_INSTEAD: return listInstead = listInstead.filter(o => o !== program)
+//     case INSERT_AFTER: return listAfter = listAfter.filter(o => o !== program)
+//   }
+// }
+
+
+const rule = {
+  id: 'PING_PONG',
+  target: 'PING',
+  position: INSERT_AFTER,
+  condition: action => action.type === 'PING',
+  consequence: store => store.dispatch({type: 'PONG'})
+}
+
+addRule(rule, {
+  addWhen: async (next, getState) => {
+    await next(action => action.type === 'START')
+    return 'ADD_RULE'
+  },
+  addUntil: async (next, getState) => {
+    await next(action => action.type === 'END')
+    return 'REAPPLY_WHEN'
+  }
+})
---
+++ src/types.js
@@ -0,0 +1,37 @@
+// @flow
+
+export type Action = { type: string }
+type GetState<State> = () => State
+type Dispatch = (action:Action) => Action
+export type Store<S> = {
+  getState: GetState<S>,
+  dispatch: Dispatch
+}
+
+type Position = 'INSERT_BEFORE' | 'INSERT_INSTEAD' | 'INSERT_AFTER'
+
+type LogicAdd = 'ADD_RULE' | 'ABORT'
+
+type LogicRemove = 'REAPPLY_WHEN' | 'REMOVE_RULE'
+
+type NextAction = (
+  condition?: (action:Action) => boolean
+) => Promise<void>
+
+type Rule<S> = {
+  id: string,
+  target: '*' | string | string[],
+  skipRule?: '*' | string | string[],
+  position?: Position,
+  zIndex?: number,
+  condition?: (action:Action,getState:GetState<S>) => boolean,
+  consequence: (store:Store<S>,action:Action) => Action | void,
+}
+
+type Options<S> = {
+  allowConcurrent?: boolean,
+  addWhen: (next:NextAction,getState:GetState<S>) => Promise<LogicAdd>,
+  addUntil: (next:NextAction,getState:GetState<S>) => Promise<LogicRemove>
+}
+
+export type AddRule<S> = (rule:Rule<S>,opt?:Options<S>) => Rule<S>
