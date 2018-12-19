5b2c5a19063c153962a503f6d5315d98eb6b99a7
manuelJung <manu.jung@relaxcommerce.com>
2018-12-20 06:15:38 +0700

    moved helpers to ruleDB

--- src/ruleDB.js
+++ src/ruleDB.js
@@ -1,5 +1,4 @@
 // @flow
-import {createContext, forEachTarget, pushByZIndex} from './helpers'
 import * as saga from './saga'
 import type {Rule, RuleContext, Position} from './types'
 
@@ -96,4 +95,55 @@ export function addLaterAddedRules(){
 
 export function getRuleContext(rule:Rule):RuleContext {
   return ruleContextList[rule.id]
+}
+
+// HELPERS
+
+function createContext(rule:Rule):RuleContext{
+  const listeners = {}
+  return {
+    rule: rule,
+    childRules: [],
+    running: 0,
+    active: false,
+    pendingWhen: false,
+    pendingUntil: false,
+    sagaStep: 0,
+    on: (e, cb) => {
+      if(!listeners[e]) listeners[e] = []
+      listeners[e].push(cb)
+    },
+    off: (e, cb) => {
+      listeners[e] = listeners[e].filter(l => l !== cb)
+    },
+    trigger: (e, payload) => {
+      if(!listeners[e]) return
+      for(i=0;i<listeners[e].length;i++){
+        const cb = listeners[e][i]
+        cb(payload)
+      }
+    },
+  }
+}
+
+function forEachTarget(target:'*' | string | string[], cb:(target:string)=>mixed){
+  if(typeof target === 'string'){
+    if(target === '*') cb('global')
+    else cb(target)
+  }
+  else {
+    for(i=0;i<target.length;i++){ cb(target[i]) }
+  }
+}
+
+function pushByZIndex(list:Rule[], rule:Rule):void{
+  const index = list.reduce((p,n,i) => {
+    if(typeof n.zIndex !== 'number' || typeof rule.zIndex !== 'number'){
+      console.warn('if multiple rules are attached to a action you have to specify the order (zIndex)', n)
+      return p
+    }
+    if(rule.zIndex < n.zIndex) return i
+    else return p
+  }, 0)
+  list.splice(index,0,rule)
 }
