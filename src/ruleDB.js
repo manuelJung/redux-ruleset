6e575297836dc50124d5122c0fddda2ce387e91e
manuelJung <manu.jung@relaxcommerce.com>
2018-12-19 10:08:44 +0700

    created remove rule

--- src/ruleDB.js
+++ src/ruleDB.js
@@ -1,26 +1,93 @@
 // @flow
 import {createContext, forEachTarget} from './helpers'
+import * as saga from './saga'
 import type {Rule, RuleContext, Position} from './types'
 
-const activeRules = {
+type ActiveRules = {
+  INSERT_BEFORE: {[ruleId:string]: Rule[]},
+  INSERT_INSTEAD: {[ruleId:string]: Rule[]},
+  INSERT_AFTER: {[ruleId:string]: Rule[]}
+}
+
+const activeRules:ActiveRules = {
   'INSERT_BEFORE': {},
   'INSERT_INSTEAD': {},
   'INSERT_AFTER': {}
 }
 
-const ruleContextList = {}
+let laterAddedRules:(()=>void)[] = []
 
-export function addRule(rule:Rule, parentRuleId:string){
+let i
+
+const ruleContextList:{[ruleId:string]: RuleContext} = {}
+
+export function addRule(rule:Rule, parentRuleId?:string):Rule{
   const context = createContext(rule)
   const position = rule.position || 'INSERT_AFTER'
-  // add context and rule
-  ruleContextList[rule.id] = context
-  forEachTarget(rule.target, target => {
-    if(!activeRules[position][target]) activeRules[position][target] = []
-    activeRules[position][target].push(rule)
+
+  if(parentRuleId){
+    const parentContext = ruleContextList[parentRuleId]
+    parentContext.childRules.push(rule)
+  }
+
+  const add = () => {
+    context.active = true
+    ruleContextList[rule.id] = context
+    forEachTarget(rule.target, target => {
+      if(!activeRules[position][target]) activeRules[position][target] = []
+      activeRules[position][target].push(rule)
+    })
+    context.trigger('ADD_RULE')
+  }
+  const addWhen = () => rule.addWhen && saga.createSaga(context, rule.addWhen, logic => {
+    switch(logic){
+      case 'ADD_RULE': laterAddedRules.push(add); break
+      case 'ADD_RULE_BEFORE': add(); break
+      case 'REAPPLY_WHEN': addWhen(); break
+    }
   })
+  const addUntil = () => rule.addUntil && saga.createSaga(context, rule.addUntil, logic => {
+    switch(logic){
+      case 'RECREATE_RULE': removeRule(rule) && addRule(rule); break
+      case 'REMOVE_RULE': removeRule(rule); break
+      case 'REAPPLY_REMOVE': addUntil(); break
+    }
+  })
+
+  if(rule.addWhen) addWhen()
+  else add()
+  return rule
 }
 
-export function removeRule(rule:Rule){}
+export function removeRule(rule:Rule){
+  const context = ruleContextList[rule.id]
+  const position = rule.position || 'INSERT_AFTER'
 
-export function forEachRuleContext(position:Position, actionType:string, cb:Function){}
+  // remove child rules before parent rule (logical order)
+  if(context.childRules.length){
+    for(i=0;i<context.childRules.length;i++){removeRule(context.childRules[i])}
+  }
+  context.active = false
+  forEachTarget(rule.target, target => {
+    const list = activeRules[position][target]
+    activeRules[position][target] = list.filter(r => r.id !== rule.id)
+  })
+  context.trigger('REMOVE_RULE')
+}
+
+export function forEachRuleContext(position:Position, actionType:string, cb:Function){
+  const globalRules = activeRules[position].global
+  const boundRules = activeRules[position][actionType]
+  if(globalRules){
+    for(i=0;i<globalRules.length;i++){cb(globalRules[i])}
+  }
+  if(boundRules){
+    for(i=0;i<boundRules.length;i++){cb(boundRules[i])}
+  }
+}
+
+export function addLaterAddedRules(){
+  if(!laterAddedRules.length) return
+  for (i=0;i<laterAddedRules.length;i++){ laterAddedRules[i]() }
+  laterAddedRules = []
+}
