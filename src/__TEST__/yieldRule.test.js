e04c305bebd6529099e3b04b76dd66a2a7f5606e
manuelJung <manu.jung@relaxcommerce.com>
2018-12-22 06:22:01 +0700

    commented old tests

--- src/__TEST__/yieldRule.test.js
+++ src/__TEST__/yieldRule.test.js
@@ -1,28 +1,28 @@
-// @flow
-import {createYielder} from '../yieldRule'
-import configureMockStore from 'redux-mock-store'
+// // @flow
+// import {createYielder} from '../yieldRule'
+// import configureMockStore from 'redux-mock-store'
 
-declare var describe: any
-declare var test: any
-declare var expect: any
-declare var jest: any
+// declare var describe: any
+// declare var test: any
+// declare var expect: any
+// declare var jest: any
 
-const mockStore = configureMockStore()
+// const mockStore = configureMockStore()
 
-test('it should handle an async workflow', async () => {
-  const cb = jest.fn()
-  const store = mockStore({})
-  const yielder = createYielder(store)
-  yielder.add(async next => {
-    await next(() => true)
-    await next()
-    console.log('2')
-    return 'RESOLVE'
-  }, cb)
-  yielder.yieldAction({type:'NEXT'})
-  expect(cb.mock.calls.length).toBe(0)
-  yielder.yieldAction({type:'NEXT'})
-  await cb()
-  expect(cb.mock.calls.length).toBe(1)
-})
+// test('it should handle an async workflow', async () => {
+//   const cb = jest.fn()
+//   const store = mockStore({})
+//   const yielder = createYielder(store)
+//   yielder.add(async next => {
+//     await next(() => true)
+//     await next()
+//     console.log('2')
+//     return 'RESOLVE'
+//   }, cb)
+//   yielder.yieldAction({type:'NEXT'})
+//   expect(cb.mock.calls.length).toBe(0)
+//   yielder.yieldAction({type:'NEXT'})
+//   await cb()
+//   expect(cb.mock.calls.length).toBe(1)
+// })
 
