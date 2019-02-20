5e5b268ea662a39adeb74669752c21f752732d6f
manuelJung <manu.jung@relaxcommerce.com>
2019-02-20 21:06:39 +0100

    created const file

---
+++ src/const.js
@@ -0,0 +1,33 @@
+// @flow
+
+
+// positions
+
+export const BEFORE:'BEFORE' = 'BEFORE'
+export const INSTEAD:'INSTEAD' = 'INSTEAD'
+export const AFTER:'AFTER' = 'AFTER'
+
+// logic add
+
+export const ADD_RULE:'ADD_RULE' = 'ADD_RULE'
+export const ABORT:'ABORT' = 'ABORT'
+export const REAPPLY_WHEN:'REAPPLY_WHEN' = 'REAPPLY_WHEN'
+export const ADD_RULE_BEFORE:'ADD_RULE_BEFORE' = 'ADD_RULE_BEFORE'
+
+// logic remove
+
+export const RECREATE_RULE:'RECREATE_RULE' = 'RECREATE_RULE'
+export const REMOVE_RULE:'REMOVE_RULE' = 'REMOVE_RULE'
+export const REMOVE_RULE_BEFORE:'REMOVE_RULE_BEFORE' = 'REMOVE_RULE_BEFORE'
+export const REAPPLY_REMOVE:'REAPPLY_REMOVE' = 'REAPPLY_REMOVE'
+export const READD_RULE:'READD_RULE' = 'READD_RULE'
+export const RECREATE_RULE_BEFORE:'RECREATE_RULE_BEFORE' = 'RECREATE_RULE_BEFORE'
+
+// concurency
+
+export const DEFAULT:'DEFAULT' = 'DEFAULT'
+export const FIRST:'FIRST' = 'FIRST'
+export const LAST:'LAST' = 'LAST'
+export const ONCE:'ONCE' = 'ONCE'
+export const SWITCH:'SWITCH' = 'SWITCH'
+export const ORDERED:'ORDERED' = 'ORDERED'
