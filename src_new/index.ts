a6f71ca38e2fb6d965d74a432da4fb276f53b89a
manuelJung <manu.jung@relaxcommerce.com>
2022-02-23 12:44:14 +0100

    api fixes

--- src_new/index.ts
+++ src_new/index.ts
@@ -56,32 +56,27 @@ export default function createMiddleware<Action extends {type:string}, RootState
       registerRuleFn.dropRule(rule)
     },
     recreateRules: (selector: string | string[]) => registerRuleFn.recreateRules(selector),
-    skipRule(ruleId:'*'|string|string[], action:t.Action) {
-      if(action.meta && typeof action.meta !== 'object') throw new Error('Expect action.meta be be an action')
-      let newAction:t.Action = {type:'-'}
-      let key:string
-      for(key in action) {newAction[key] = action[key]}
-      if(!newAction.meta) newAction.meta = {}
-      else for (key in action.meta) {newAction.meta[key] = action.meta[key]}
-    
-      if(!newAction.meta.skipRule) newAction.meta.skipRule = ruleId
-      else if(newAction.meta.skipRule === '*' || ruleId === '*') newAction.meta.skipRule = '*'
-      else if (typeof newAction.meta.skipRule === 'string'){
-        if(typeof ruleId === 'string') newAction.meta.skipRule = [ruleId, newAction.meta.skipRule]
-        else newAction.meta.skipRule = [...ruleId, newAction.meta.skipRule]
-      }
-      else if(Array.isArray(newAction.meta.skipRule)) {
-        if(typeof ruleId === 'string') newAction.meta.skipRule = [ruleId, ...newAction.meta.skipRule]
-        else newAction.meta.skipRule = [...ruleId, ...newAction.meta.skipRule]
-      }
-      return newAction
-    }
+    skipRule: skipRule
   }
 }
 
-const middleware = store => next => action => {
-  console.log('dispatching', action)
-  let result = next(action)
-  console.log('next state', store.getState())
-  return result
+export function skipRule(ruleId:'*'|string|string[], action:t.Action) {
+  if(action.meta && typeof action.meta !== 'object') throw new Error('Expect action.meta be be an action')
+  let newAction:t.Action = {type:'-'}
+  let key:string
+  for(key in action) {newAction[key] = action[key]}
+  if(!newAction.meta) newAction.meta = {}
+  else for (key in action.meta) {newAction.meta[key] = action.meta[key]}
+
+  if(!newAction.meta.skipRule) newAction.meta.skipRule = ruleId
+  else if(newAction.meta.skipRule === '*' || ruleId === '*') newAction.meta.skipRule = '*'
+  else if (typeof newAction.meta.skipRule === 'string'){
+    if(typeof ruleId === 'string') newAction.meta.skipRule = [ruleId, newAction.meta.skipRule]
+    else newAction.meta.skipRule = [...ruleId, newAction.meta.skipRule]
+  }
+  else if(Array.isArray(newAction.meta.skipRule)) {
+    if(typeof ruleId === 'string') newAction.meta.skipRule = [ruleId, ...newAction.meta.skipRule]
+    else newAction.meta.skipRule = [...ruleId, ...newAction.meta.skipRule]
+  }
+  return newAction
 }
